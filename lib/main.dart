e8ce6ac64e7d5b3669417b4b7754f7b4b67164dc
Kemalsheker <kemal@marketyo.com>
2022-04-11 16:48:19 +0300

    Handle internet connections changes, disable landscape mode

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,21 @@
+import 'dart:async';
+import 'dart:io';
+
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:winternews/newsItem.dart';
 import 'package:winternews/rss_to_json.dart';
 import 'package:winternews/sort_pubDate.dart';
 import 'package:winternews/webview.dart';
-import 'package:xml/xml.dart';
 import 'package:shimmer/shimmer.dart';
 import 'package:auto_size_text/auto_size_text.dart';
+import 'package:flutter/services.dart';
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  SystemChrome.setPreferredOrientations(
+      [DeviceOrientation.portraitUp, DeviceOrientation.portraitDown]);
+
   runApp(const MyApp());
 }
 
@@ -44,7 +51,6 @@ class MyGridView extends StatefulWidget {
   const MyGridView({Key? key}) : super(key: key);
 
 
-
   @override
   State<StatefulWidget> createState() => _MyGridViewState();
 
@@ -54,16 +60,30 @@ class _MyGridViewState extends State<MyGridView> {
 
   List<NewsItem> newsData = [];
   bool isLoading = true;
+  bool isInternetConnected = false;
 
 
-  void _addNewsToList(List news){
-    for(NewsItem i in news){
+  void _addNewsToList(List news) {
+    for (NewsItem i in news) {
       newsData.add(i);
     }
   }
 
 
-  getData() async{
+  internetConnected() async {
+    try {
+      final result = await InternetAddress.lookup('example.com');
+      if (result.isNotEmpty && result[0].rawAddress.isNotEmpty) {
+        isInternetConnected = true;
+      }
+    } on SocketException catch (_) {
+      isInternetConnected = false;
+    }
+  }
+
+
+  getData() async {
+    dialogAfterTimeOut();
     Future.wait([
       rssToJson('https://thesnowmag.com/feed/', 'The Snow Mag'),
       rssToJson('https://www.inthesnow.com/feed/', 'InTheSnow'),
@@ -73,7 +93,8 @@ class _MyGridViewState extends State<MyGridView> {
       rssToJson('https://etusuora.com/rss/en/rss_winter_sports.xml', 'ETUSUORA'),
       rssToJson('https://www.snowboarder.com/feed/', 'Snowboarder'),
       rssToJson('http://www.agnarchy.com/feed/', 'Agnarchy'),
-      rssToJson('https://planetski.eu/feed/', 'PlanetSKI')
+      rssToJson('https://planetski.eu/feed/', 'PlanetSKI'),
+      rssToJson('https://www.theavantski.com/blog-feed.xml', 'Avant Ski')
     ]).then((value) {
       _addNewsToList(value[0]);
       _addNewsToList(value[1]);
@@ -84,6 +105,7 @@ class _MyGridViewState extends State<MyGridView> {
       _addNewsToList(value[6]);
       _addNewsToList(value[7]);
       _addNewsToList(value[8]);
+      _addNewsToList(value[9]);
 
       sort_pubDate(newsData);
       setState(() {
@@ -93,81 +115,113 @@ class _MyGridViewState extends State<MyGridView> {
   }
 
 
-  @override
-  void initState() {
-    // TODO: implement initState
-    super.initState();
-    getData();
+  void dialogAfterTimeOut() {
+    Timer _timer = Timer(const Duration(seconds: 20), () {
+      if (isLoading) {
+       showDialog(context: context, builder: (context) => AlertDialog(
+           title: const Text('Error Loading the News!'),
+           content: const Text('Check your internet connectivity'),
+           actions: [
+             FlatButton(onPressed: () => exit(0) , child: const Text('Close')),
+             FlatButton(onPressed: () {
+                                  internetConnected();
+                                  getData();
+                                  Navigator.pop(context);
+                                  },
+                        child: const Text('Retry'))
+           ],
+       ));
+      }
+    });
   }
 
-  @override
-  Widget build(BuildContext context) {
-    if(isLoading){
-      return  Container(
-        alignment: Alignment.center,
-        width: 600.0,
-        height: 600.0,
-        child: Shimmer.fromColors(
+
+@override
+void initState() {
+  // TODO: implement initState
+  super.initState();
+  internetConnected();
+  getData();
+}
+
+
+@override
+Widget build(BuildContext context) {
+  if (isLoading) {
+    return Container(
+      alignment: Alignment.center,
+      width: 600.0,
+      height: 600.0,
+      child: Shimmer.fromColors(
           baseColor: Colors.indigo,
           highlightColor: Colors.white,
           child: Image.asset('images/snowflake.png', fit: BoxFit.fill)
-        ),
-      );
-    }
-    else{
-      return GridView.builder(
-          padding: const EdgeInsets.all(8.0),
-          itemCount: newsData.length,
-          gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
-              maxCrossAxisExtent: MediaQuery. of(context). size. height / 3,
-              childAspectRatio: 1,
-              crossAxisSpacing: 20,
-              mainAxisSpacing: 20),
-          itemBuilder: (BuildContext ctx, index) {
-            return Ink(
-                decoration: BoxDecoration(color: Colors.white, borderRadius: BorderRadius.circular(10)),
-                child: InkWell(
-                    child: Container(
-                      alignment: Alignment.centerLeft,
-                      padding: const EdgeInsets.all(8.0),
-                      child: Column(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        crossAxisAlignment: CrossAxisAlignment.center,
-                        children: <Widget>[
-                          AutoSizeText(
-                              newsData[index].getTitle,
-                              style:  const TextStyle(fontSize: 18.0),
-                              maxLines: 5 ,),
-                          const SizedBox(height: 10),
-                          Row(
-                            mainAxisAlignment: MainAxisAlignment.spaceAround,
-                            crossAxisAlignment: CrossAxisAlignment.end,
-                            children: <Widget>[
-                              AutoSizeText(newsData[index].pubName, style: const TextStyle(fontSize: 4.0), maxLines: 2),
-                              AutoSizeText(newsData[index].pubDate, style: const TextStyle(fontSize: 4.0), maxLines: 2)
-                            ],
-                          )
-                        ],
-                      ),
-                    ),
-                    onTap: () {
-                      Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                              builder: (context) =>
-                                  MyWebView(
-                                    selectedUrl: newsData[index].link,
-                                  )
-                          )
-                      );
-                    }
-                )
-            );
-          });
-    }
-
+      ),
+    );
   }
-}
+  else {
+    return GridView.builder(
+        padding: const EdgeInsets.all(8.0),
+        itemCount: newsData.length,
+        gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
+            maxCrossAxisExtent: MediaQuery
+                .of(context)
+                .size
+                .height / 3,
+            childAspectRatio: 1,
+            crossAxisSpacing: 20,
+            mainAxisSpacing: 20),
+        itemBuilder: (BuildContext ctx, index) {
+          return Ink(
+              decoration: BoxDecoration(
+                  color: Colors.white, borderRadius: BorderRadius.circular(10)),
+              child: InkWell(
+                  child: Container(
+                    alignment: Alignment.centerLeft,
+                    padding: const EdgeInsets.all(8.0),
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: <Widget>[
+                        AutoSizeText(
+                          newsData[index].getTitle,
+                          style: const TextStyle(fontSize: 18.0),
+                          maxLines: 5,),
+                        const SizedBox(height: 10),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceAround,
+                          crossAxisAlignment: CrossAxisAlignment.end,
+                          children: <Widget>[
+                            AutoSizeText(newsData[index].pubName,
+                                style: const TextStyle(fontSize: 4.0),
+                                maxLines: 2),
+                            AutoSizeText(newsData[index].pubDate,
+                                style: const TextStyle(fontSize: 4.0),
+                                maxLines: 2)
+                          ],
+                        )
+                      ],
+                    ),
+                  ),
+                  onTap: () {
+                    Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                            builder: (context) =>
+                                MyWebView(
+                                  selectedUrl: newsData[index].link,
+                                )
+                        )
+                    );
+                  }
+              )
+          );
+        });
+  }
+}}
+
+
+
 
 
 
